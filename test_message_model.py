15dab047ba3f50470d1f92069bde2f6a16642048
maxhirtens <max@maxhirtenstein.com>
2022-11-17 12:21:53 -0800

    testing pt2

--- test_message_model.py
+++ test_message_model.py
@@ -2,12 +2,13 @@
 
 # run these tests like:
 #
-#    python3 -m unittest test_message_model.py
+#    python -m unittest test_message_model.py
+
 
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, Likes
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -26,3 +27,66 @@ from app import app
 # and create fresh new clean test data
 
 db.create_all()
+
+
+class UserModelTestCase(TestCase):
+    """Test message model."""
+
+    def setUp(self):
+        """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
+
+        self.uid = 94566
+        u = User.signup("testing", "testing@test.com", "password", None)
+        u.id = self.uid
+        db.session.commit()
+
+        self.u = User.query.get(self.uid)
+
+        self.client = app.test_client()
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
+    def test_message_model(self):
+        """Does basic model work?"""
+
+        m = Message(
+            text="boomerang",
+            user_id=self.uid
+        )
+
+        db.session.add(m)
+        db.session.commit()
+
+        # User should have 1 message
+        self.assertEqual(len(self.u.messages), 1)
+        self.assertEqual(self.u.messages[0].text, "boomerang")
+
+    def test_message_likes(self):
+        m1 = Message(
+            text="boomerang",
+            user_id=self.uid
+        )
+
+        m2 = Message(
+            text="another boomerang",
+            user_id=self.uid
+        )
+
+        u = User.signup("yetanothertest", "t@email.com", "password", None)
+        uid = 888
+        u.id = uid
+        db.session.add_all([m1, m2, u])
+        db.session.commit()
+
+        u.likes.append(m1)
+
+        db.session.commit()
+
+        l = Likes.query.filter(Likes.user_id == uid).all()
+        self.assertEqual(len(l), 1)
+        self.assertEqual(l[0].message_id, m1.id)
--- test_user_model.py
+++ test_user_model.py
@@ -2,12 +2,11 @@
 
 # run these tests like:
 #
-#    python3 -m unittest test_user_model.py
+#    python -m unittest test_user_model.py
 
 
 import os
 from unittest import TestCase
-
 from models import db, User, Message, Follows
 
 # BEFORE we import our app, let's set an environmental variable
@@ -34,12 +33,35 @@ class UserModelTestCase(TestCase):
 
     def setUp(self):
         """Create test client, add sample data."""
+        db.drop_all()
+        db.create_all()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+        u1 = User.signup("test1", "email1@email.com", "password", None)
+        uid1 = 1111
+        u1.id = uid1
+
+        u2 = User.signup("test2", "email2@email.com", "password", None)
+        uid2 = 2222
+        u2.id = uid2
+
+        db.session.commit()
+
+        u1 = User.query.get(uid1)
+        u2 = User.query.get(uid2)
+
+        self.u1 = u1
+        self.uid1 = uid1
+
+        self.u2 = u2
+        self.uid2 = uid2
 
         self.client = app.test_client()
+
+    def tearDown(self):
+        res = super().tearDown()
+        db.session.rollback()
+        return res
+
 
     def test_user_model(self):
         """Does basic model work?"""
@@ -57,5 +79,41 @@ class UserModelTestCase(TestCase):
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
 
-        # does repr look right?
-        self.assertEqual(f'{u}', f'<User #{u.id}: {u.username}, {u.email}>')
+    ####
+    # Following tests
+    ####
+    def test_user_follows(self):
+        self.u1.following.append(self.u2)
+        db.session.commit()
+
+        self.assertEqual(len(self.u2.following), 0)
+        self.assertEqual(len(self.u2.followers), 1)
+        self.assertEqual(len(self.u1.followers), 0)
+        self.assertEqual(len(self.u1.following), 1)
+
+        self.assertEqual(self.u2.followers[0].id, self.u1.id)
+        self.assertEqual(self.u1.following[0].id, self.u2.id)
+
+    ####
+    # Signup Tests
+    ####
+    def test_valid_signup(self):
+        u_test = User.signup("testtesttest", "testtest@test.com", "password", None)
+        uid = 99999
+        u_test.id = uid
+        db.session.commit()
+
+        u_test = User.query.get(uid)
+        self.assertIsNotNone(u_test)
+        self.assertEqual(u_test.username, "testtesttest")
+        self.assertEqual(u_test.email, "testtest@test.com")
+        self.assertNotEqual(u_test.password, "password")
+        # Bcrypt strings should start with $2b$
+        self.assertTrue(u_test.password.startswith("$2b$"))
+
+    ####
+    # Authentication Tests
+    ####
+
+    def test_wrong_password(self):
+        self.assertFalse(User.authenticate(self.u1.username, "badpassword"))
